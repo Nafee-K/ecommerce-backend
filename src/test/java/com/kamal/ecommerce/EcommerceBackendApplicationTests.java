a9ff2fc5ea6c337f95ec4b83cce604aba5eab6c1
Nafee <24286185+Nafee-K@users.noreply.github.com>
2026-05-08 06:02:07 -0400

    Initial Spring Boot project setup

---
+++ src/test/java/com/kamal/ecommerce/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
